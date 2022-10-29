1110f87104fe296938ba68fb863f21f2bcf39555
renanfigueoli <renanfigueoli@outlook.com>
2022-10-29 01:47:27 -0300

    inicio do autoSugest

--- src/Pages/Produto/CadastroDeProduto/index.tsx
+++ src/Pages/Produto/CadastroDeProduto/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Cabecalho from '../../../Components/Cabecalho';
 import Footer from '../../../Components/Footer';
-import { Button, TextField } from "@mui/material";
-
+import { Button, TextField, Autocomplete, Stack } from "@mui/material";
+import IFornecedor from '../../../interfaces/IFornecedor';
+import apiFullSports from '../../../api/apiFullSports';
 const Main = styled.main`
     width: 100%;
     min-height: 600px;
@@ -49,83 +50,105 @@ const BttCadPrdutoGrid = styled.div`
     grid-auto-rows: minmax(auto, auto);
     grid-gap: 2px;
 `;
-const CadastrarProduto = () =>{
+
+
+const CadastrarProduto = () => {
     const date = new Date().toLocaleDateString()
-    return(
+    const [listaFornecedores, setListaFornecedores] = useState<IFornecedor[]>([]);
+    const [fornecedorID, setFornecedorID] = useState('')
+    const [fornecedor, setFornecedor] = useState('')
+    
+    useEffect(() => {
+        apiFullSports.get<IFornecedor[]>('listar-fornecedores/')
+            .then(resposta => setListaFornecedores(resposta.data))
+    }, [])
+    console.log(listaFornecedores)
+    const options = listaFornecedores.map((item) => {
+        const firsLetter = item.nomeEmpresa[0].toLocaleUpperCase();
+        const firsLetterId = item._id
+        return {
+            firsLetter: /[0-9]/.test(firsLetter) ? '0-9' : firsLetter,
+            ...item,
+            firsLetterId : firsLetterId
+        };
+    })
+
+  
+    return (
         <>
-        <Cabecalho />
-        <Main>
-            <ExibeTitulo id="exibe-titulo" className="exibe-titulo" >Cadastrar Produto</ExibeTitulo>
-            <FormCadastroDeProduto id="form-cadastro-produto" className="form-cadastro-produto">
-            <form action="" method="post" encType="multipart/form-data">
-                <Row2grid id="row-2-grid" className="row-1-grid">
-                    <label className="col-form-label">Nome do Produto</label>
-                    <TextField
-                        sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
-                        className="txt-form"
-                        label="Nome do Produto"
-                        id="nomeProduto"
-                        type="text"
-                        placeholder={'Digite o nome do produto'}
-                        fullWidth
-                    />
+            <Cabecalho />
+            <Main>
+                <ExibeTitulo id="exibe-titulo" className="exibe-titulo" >Cadastrar Produto</ExibeTitulo>
+                <FormCadastroDeProduto id="form-cadastro-produto" className="form-cadastro-produto">
+                    <form action="" method="post" encType="multipart/form-data">
+                        <Row2grid id="row-2-grid" className="row-1-grid">
+                            <label className="col-form-label">Nome do Produto</label>
+                            <TextField
+                                sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
+                                className="txt-form"
+                                label="Nome do Produto"
+                                id="nomeProduto"
+                                type="text"
+                                placeholder={'Digite o nome do produto'}
+                                fullWidth
+                            />
 
-                    <label className="col-form-label">Tipo do Produto</label>
-                    <TextField
-                        sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
-                        className="txt-form"
-                        label="Tipo do Produto"
-                        id="tipoProduto"
-                        type="text"
-                        placeholder={'Digite o tipo do produto'}
-                        fullWidth
-                    />
+                            <label className="col-form-label">Tipo do Produto</label>
+                            <TextField
+                                sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
+                                className="txt-form"
+                                label="Tipo do Produto"
+                                id="tipoProduto"
+                                type="text"
+                                placeholder={'Digite o tipo do produto'}
+                                fullWidth
+                            />
 
-                    <label className="col-form-label">Cor do Produto</label>
-                    <TextField
-                        sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
-                        className="txt-form"
-                        label="Cor do Produto"
-                        id="corProduto"
-                        type="text"
-                        placeholder={'Digite o tipo a cor do produto'}
-                        fullWidth
-                    />
+                            <label className="col-form-label">Cor do Produto</label>
+                            <TextField
+                                sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
+                                className="txt-form"
+                                label="Cor do Produto"
+                                id="corProduto"
+                                type="text"
+                                placeholder={'Digite o tipo a cor do produto'}
+                                fullWidth
+                            />
 
-                    <label className="col-form-label">Preço do Produto</label>
-                    <TextField
-                        sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
-                        className="txt-form"
-                        label="Preço do Produto"
-                        id="precoProduto"
-                        type="text"
-                        placeholder={'R$:'}
-                        fullWidth
-                    />
-                    <label className="col-form-label">Quantidade de Produtos</label>
-                    <TextField
-                        sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
-                        className="txt-form"
-                        label="quantidade"
-                        id="qtdProduto"
-                        type="number"
-                        // placeholder={'Digite a quantidade de Produto'}
-                        fullWidth
-                    />
+                            <label className="col-form-label">Preço do Produto</label>
+                            <TextField
+                                sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
+                                className="txt-form"
+                                label="Preço do Produto"
+                                id="precoProduto"
+                                type="text"
+                                placeholder={'R$:'}
+                                fullWidth
+                            />
+                            <label className="col-form-label">Quantidade de Produtos</label>
+                            <TextField
+                                sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
+                                className="txt-form"
+                                label="quantidade"
+                                id="qtdProduto"
+                                type="number"
+                                // placeholder={'Digite a quantidade de Produto'}
+                                fullWidth
+                            />
 
-                    <label className="col-form-label">Data de Cadastro</label>
-                    <TextField
-                        sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%', textAlign: 'center'}}
-                        className="txt-form"
-                        label="Data"
-                        id="tipoProduto"
-                        value={date}
-                        type="text"
-                        // placeholder={'Digite o tipo do produto'}
-                        fullWidth
-                    />
-                    <label className="col-form-label">CNPJ do Fornecedor</label>
-                    <TextField
+                            <label className="col-form-label">Data de Cadastro</label>
+                            <TextField
+                                sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%', textAlign: 'center' }}
+                                className="txt-form"
+                                label="Data"
+                                id="tipoProduto"
+                                value={date}
+                                type="text"
+                                // placeholder={'Digite o tipo do produto'}
+                                fullWidth
+                            />
+                            <label className="col-form-label">CNPJ do Fornecedor</label>
+                            {/* <TextField
                         sx={{ boxSizing: 'border-box', margin: '0 0 15px', width: '100%' }}
                         className="txt-form"
                         label="CNPJ"
@@ -133,33 +156,44 @@ const CadastrarProduto = () =>{
                         type="text"
                         placeholder={'00.000.000./0000-00'}
                         fullWidth
-                    />
+                    /> */}
+                            <Autocomplete
+                                id="grouped-demo"
+                                options={options.sort((a, b) => -b.firsLetter.localeCompare(a.firsLetter))}
+                                groupBy={(option) => option.firsLetter}
+                                getOptionLabel={(option) => option.nomeEmpresa+' - '+ option.cnpj}
+                                onInputChange={(event, newInputValue,option) => {
+                                    setFornecedorID(option);
+                                  }}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField {...params} label="Nome/Cnpj" />}
+                            />
+                           
+                        </Row2grid>
 
-                </Row2grid>
 
-
-                <BttCadPrdutoGrid id="btt-cad-cliente-grid" className="btt-cad-cliente-grid">
-                <Button
-                        sx={{
-                            justifyContent: 'center', display: 'block', height: '50px', borderRadius: '5px', color: '#fff',
-                            fontSize: '14px', backgroundColor: 'black', ":hover": 'backgroundColor: #313131, transform:translate(0.8s)'
-                        }}
-                        type="submit" id="btn-cad-forms" className="btn-cad-forms">
-                        Cadastrar Produto
-                    </Button>
-                    <Button
-                        onClick={evento => window.open('/sig/consulta-de-produtos')}
-                        sx={{
-                            justifyContent: 'center', display: 'block', height: '50px', borderRadius: '5px', color: '#fff',
-                            fontSize: '14px', backgroundColor: 'black', ":hover": 'backgroundColor: #313131, transform:translate(0.8s)'
-                        }} type="button" id="btn-cad-forms" className="btn-cad-forms">
-                        Consulta de Produto
-                    </Button>
-                </BttCadPrdutoGrid>
-            </form>
-        </FormCadastroDeProduto>
-        </Main>
-        <Footer />
+                        <BttCadPrdutoGrid id="btt-cad-cliente-grid" className="btt-cad-cliente-grid">
+                            <Button
+                                sx={{
+                                    justifyContent: 'center', display: 'block', height: '50px', borderRadius: '5px', color: '#fff',
+                                    fontSize: '14px', backgroundColor: 'black', ":hover": 'backgroundColor: #313131, transform:translate(0.8s)'
+                                }}
+                                type="submit" id="btn-cad-forms" className="btn-cad-forms">
+                                Cadastrar Produto
+                            </Button>
+                            <Button
+                                onClick={evento => window.open('/sig/consulta-de-produtos')}
+                                sx={{
+                                    justifyContent: 'center', display: 'block', height: '50px', borderRadius: '5px', color: '#fff',
+                                    fontSize: '14px', backgroundColor: 'black', ":hover": 'backgroundColor: #313131, transform:translate(0.8s)'
+                                }} type="button" id="btn-cad-forms" className="btn-cad-forms">
+                                Consulta de Produto
+                            </Button>
+                        </BttCadPrdutoGrid>
+                    </form>
+                </FormCadastroDeProduto>
+            </Main>
+            <Footer />
         </>
     );
 };
