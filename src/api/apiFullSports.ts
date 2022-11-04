14429f83754cf8520956f88da538911ca097a1c3
renanfigueoli <renanfigueoli@outlook.com>
2022-11-04 18:31:49 -0300

    teste apiHeroku

--- src/api/apiFullSports.ts
+++ src/api/apiFullSports.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiFullSports = axios.create({
-    baseURL: "http://localhost:5000/"
+    baseURL: process.env.APP_URL || "http://localhost:5000/"
 });
 apiFullSports.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export default apiFullSports;
