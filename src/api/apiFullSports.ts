c2a9cab2050b9d1ea2db533b13bab0ad44dbb008
renanfigueoli09 <renanfigueoli@outlook.com>
2022-11-05 08:30:02 -0300

    baseUrl

--- src/api/apiFullSports.ts
+++ src/api/apiFullSports.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const url = process.env.APP_URL || "http://localhost:5000/"
 const apiFullSports = axios.create({
-    baseURL: "https://backend-fullsport.herokuapp.com"
+    baseURL: "https://backend-fullsport.herokuapp.com/"
 });
 apiFullSports.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export default apiFullSports;
