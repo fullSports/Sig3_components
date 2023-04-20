0c179e7b05c1c6abb46c708618ab6921dd044d1f
RenanFigueiredoEdglobo <rfdoliveira@ogmaster.local>
2023-04-19 23:58:53 -0300

    teste deploy vercel

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ThemeContextProvider from './utils/Hooks/useTheme';
-
+import { Analytics } from '@vercel/analytics/react';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ThemeContextProvider>
-    <App />
-  </ThemeContextProvider>
+  <>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+    <Analytics />
+  </>
 );
