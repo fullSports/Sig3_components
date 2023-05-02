e16992193903acf8107f923d0aa2fc9080d337cc
rfdoliveira@ogmaster.local - <rfdoliveira@ogmaster.local>
2023-05-02 20:43:43 -0300

    svg spinner na home

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { render, screen } from '@testing-library/react'
+// import { render, screen } from '@testing-library/react';
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-})
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+});
