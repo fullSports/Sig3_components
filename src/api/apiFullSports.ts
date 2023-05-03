a68271b637b458dcb51e45c7a4e1595759d8aeec
renanfigueoli09 <renanfigueoli@outlook.com>
2023-05-03 02:40:44 -0300

    update vercel

--- src/api/apiFullSports.ts
+++ src/api/apiFullSports.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //https://back-end-full-sports.vercel.app/
 const apiFullSports = axios.create({
-	baseURL: 'http://localhost:5000/',
+	baseURL: 'https://back-end-full-sports.vercel.app/',
 });
 apiFullSports.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export default apiFullSports;
